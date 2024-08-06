88883ccd3c697a82b7803a16201396918f427510
AzumaMushintsuki <azuma.mushintsuki@gmail.com>
2024-08-06 13:42:58 +0300

    hw1 finished

---
+++ hw1/hw1.js
@@ -0,0 +1,40 @@
+//----task-1-----//
+let word1 = 'hello';
+let word2 = 'owu';
+let word3 = 'com';
+let word4 = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let num5 = 3.14;
+let num6 = 2.7;
+let num7 = 16;
+let bool1 = true;
+let bool2 = false;
+console.log(word1)
+console.log(word2)
+console.log(word3)
+console.log(word4)
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+console.log(num5)
+console.log(num6)
+console.log(num7)
+console.log(bool1)
+console.log(bool2)
+//------task-2-------//
+let firstName = "Анатолій"
+let middleName = "Анатолійович"
+let lastName = "Мушинський"
+let person = `${firstName} ${middleName} ${lastName}`
+console.log(person)
+//------task-3--------//
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
