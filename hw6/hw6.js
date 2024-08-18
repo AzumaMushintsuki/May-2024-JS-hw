81a05bf34ed2157b3f050bcb4938fc14a59777fe
AzumaMushintsuki <azuma.mushintsuki@gmail.com>
2024-08-18 12:33:55 +0300

    hw6 finished

--- hw6/hw6.js
+++ hw6/hw6.js
@@ -41,13 +41,14 @@ function sortNums(array, direction) {
     }
 }
 
-let coursesAndDurationArray = [{title: 'JavaScript Complex', monthDuration: 5}, {
-    title: 'Java Complex',
-    monthDuration: 6
-}, {title: 'Python Complex', monthDuration: 6}, {title: 'QA Complex', monthDuration: 4}, {
-    title: 'FullStack',
-    monthDuration: 7
-}, {title: 'Frontend', monthDuration: 4}];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 const cADA = coursesAndDurationArray
     .sort((a, b) => b.monthDuration - a.monthDuration)
     .filter(value => value.monthDuration > 5)
@@ -67,67 +68,43 @@ const cADA = coursesAndDurationArray
 //     }
 // }
 // console.log(cards)
-const cards = [{cardSuit: 'spade', value: '6', color: 'black'}, {
-    cardSuit: 'spade',
-    value: '7',
-    color: 'black'
-}, {cardSuit: 'spade', value: '8', color: 'black'}, {cardSuit: 'spade', value: '9', color: 'black'}, {
-    cardSuit: 'spade',
-    value: '10',
-    color: 'black'
-}, {cardSuit: 'spade', value: 'jack', color: 'black'}, {
-    cardSuit: 'spade',
-    value: 'queen',
-    color: 'black'
-}, {cardSuit: 'spade', value: 'king', color: 'black'}, {
-    cardSuit: 'spade',
-    value: 'ace',
-    color: 'black'
-}, {cardSuit: 'diamond', value: '6', color: 'red'}, {
-    cardSuit: 'diamond',
-    value: '7',
-    color: 'red'
-}, {cardSuit: 'diamond', value: '8', color: 'red'}, {
-    cardSuit: 'diamond',
-    value: '9',
-    color: 'red'
-}, {cardSuit: 'diamond', value: '10', color: 'red'}, {
-    cardSuit: 'diamond',
-    value: 'jack',
-    color: 'red'
-}, {cardSuit: 'diamond', value: 'queen', color: 'red'}, {
-    cardSuit: 'diamond',
-    value: 'king',
-    color: 'red'
-}, {cardSuit: 'diamond', value: 'ace', color: 'red'}, {cardSuit: 'heart', value: '6', color: 'red'}, {
-    cardSuit: 'heart',
-    value: '7',
-    color: 'red'
-}, {cardSuit: 'heart', value: '8', color: 'red'}, {cardSuit: 'heart', value: '9', color: 'red'}, {
-    cardSuit: 'heart',
-    value: '10',
-    color: 'red'
-}, {cardSuit: 'heart', value: 'jack', color: 'red'}, {
-    cardSuit: 'heart',
-    value: 'queen',
-    color: 'red'
-}, {cardSuit: 'heart', value: 'king', color: 'red'}, {
-    cardSuit: 'heart',
-    value: 'ace',
-    color: 'red'
-}, {cardSuit: 'clubs', value: '6', color: 'black'}, {cardSuit: 'clubs', value: '7', color: 'black'}, {
-    cardSuit: 'clubs',
-    value: '8',
-    color: 'black'
-}, {cardSuit: 'clubs', value: '9', color: 'black'}, {
-    cardSuit: 'clubs',
-    value: '10',
-    color: 'black'
-}, {cardSuit: 'clubs', value: 'jack', color: 'black'}, {
-    cardSuit: 'clubs',
-    value: 'queen',
-    color: 'black'
-}, {cardSuit: 'clubs', value: 'king', color: 'black'}, {cardSuit: 'clubs', value: 'ace', color: 'black'}];
+const cards = [
+    {cardSuit: 'spade', value: '6', color: 'black'},
+    {cardSuit: 'spade', value: '7', color: 'black'},
+    {cardSuit: 'spade', value: '8', color: 'black'},
+    {cardSuit: 'spade', value: '9', color: 'black'},
+    {cardSuit: 'spade', value: '10', color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+    {cardSuit: 'diamond', value: '6', color: 'red'},
+    {cardSuit: 'diamond', value: '7', color: 'red'},
+    {cardSuit: 'diamond', value: '8', color: 'red'},
+    {cardSuit: 'diamond', value: '9', color: 'red'},
+    {cardSuit: 'diamond', value: '10', color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+    {cardSuit: 'heart', value: '6', color: 'red'},
+    {cardSuit: 'heart', value: '7', color: 'red'},
+    {cardSuit: 'heart', value: '8', color: 'red'},
+    {cardSuit: 'heart', value: '9', color: 'red'},
+    {cardSuit: 'heart', value: '10', color: 'red'},
+    {cardSuit: 'heart', value: 'jack', color: 'red'},
+    {cardSuit: 'heart', value: 'queen', color: 'red'},
+    {cardSuit: 'heart', value: 'king', color: 'red'},
+    {cardSuit: 'heart', value: 'ace', color: 'red'},
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'}];
 console.log(cards.find(card => card.cardSuit === 'spade' && card.value === 'ace'));
 console.log(cards.filter(card => card.value === '6'));
 console.log(cards.filter(card => card.color === 'red'));
